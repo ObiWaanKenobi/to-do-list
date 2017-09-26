7a60b10febbc3e627c9a961e74c49628efc612cb
ObiWaanKenobi <zenua_raikkonen@mail.ru>
2017-09-26 23:08:15 +0300

    gitignore

--- src/by/gsu/epamlab/controller/controllers/task/ShowTasksController.java
+++ src/by/gsu/epamlab/controller/controllers/task/ShowTasksController.java
@@ -2,7 +2,6 @@ package by.gsu.epamlab.controller.controllers.task;
 
 import by.gsu.epamlab.model.beans.Task;
 import by.gsu.epamlab.model.exceptions.TaskDaoException;
-import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
 import static by.gsu.epamlab.controller.constants.Constants.*;
